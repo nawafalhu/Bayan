f94643c41a14451c429101f9f70baaf4f2b442fd
Musaad Almansour <443101795@student.ksu.edu.sa>
2025-02-05 22:53:24 +0300

    update interface

--- BayanProject/main/views.py
+++ BayanProject/main/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django import forms
 
 @login_required
 def home(request):
@@ -11,13 +13,26 @@ def home(request):
 def sign_practice(request):
     return render(request, 'main/sign_practice.html')
 
+class CustomUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Remove the help_texts
+        self.fields['password1'].help_text = ''
+        self.fields['password2'].help_text = ''
+        
+        # Customize labels (optional)
+        self.fields['username'].label = 'Username'
+        self.fields['password1'].label = 'Password'
+        self.fields['password2'].label = 'Confirm Password'
+
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)  # Automatically log the user in after signup
-            return redirect('home')
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'main/signup.html', {'form': form})
