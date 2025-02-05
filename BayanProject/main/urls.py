6ecb933a7c9c7d7cc7f00cf927e1ce319c983b89
MohamadAlsharhan <443101757@student.ksu.edu.sa>
2025-02-05 22:16:03 +0300

    second commit

--- BayanProject/main/urls.py
+++ BayanProject/main/urls.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LogoutView
 from django.contrib import admin
-from django.urls import path
-from main import views
+from django.urls import path , include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- BayanProject/main/views.py
+++ BayanProject/main/views.py
@@ -2,6 +2,23 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm, RegistrationForm
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = LoginForm()
+
+    return render(request, 'login.html', {'form': form})
 
 @login_required
 def home(request):
@@ -13,11 +30,11 @@ def sign_practice(request):
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)  # Automatically log the user in after signup
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = RegistrationForm()
     return render(request, 'main/signup.html', {'form': form})
